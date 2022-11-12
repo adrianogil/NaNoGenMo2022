b37f23c14cb6c5b43ee6f364564c45487e22b385
Adriano Gil <adrianogil.san@gmail.com>
2022-11-12 19:06:06 -0400

    Add a new test to check command using subprocess

--- tests/integrationtests/storygeneration/test_cli.py
+++ tests/integrationtests/storygeneration/test_cli.py
@@ -1,9 +1,16 @@
 from nanogenmo2022.storygeneration.cli import cli
 
+import subprocess
 
-def test_cli(capsys):
+
+def test_cli_output(capsys):
     args = []
     cli(args)
     captured = capsys.readouterr()
     result = captured.out
     assert "Start genering a new story!" in result
+
+
+def test_cli_output_subprocess():
+    result = subprocess.run(["generate"], capture_output=True)
+    assert b"Start genering a new story!" in result.stdout
